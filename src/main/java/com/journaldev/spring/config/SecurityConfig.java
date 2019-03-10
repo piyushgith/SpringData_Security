60ca765c57162ff7e37207e88b754cef9b11e799
Piyush <piyushprasad2007@gmail.com>
2019-03-10 18:08:48 +0530

    Added Spring Security with default login page

---
+++ src/main/java/com/journaldev/spring/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.journaldev.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().anyRequest().authenticated().and().formLogin().and().httpBasic(); //This will validate each & every request
+
+		// http.authorizeRequests().antMatchers("/person/**").authenticated().and().formLogin().permitAll();//this will look for pattern
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("user").password("password").roles("USER")
+		.and().withUser("admin").password("password").roles("USER", "ADMIN");
+	}
+}
---
+++ src/main/java/com/journaldev/spring/config/SecurityWebInitializer.java
@@ -0,0 +1,18 @@
+package com.journaldev.spring.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebInitializer extends AbstractSecurityWebApplicationInitializer {
+
+	/*
+	Alternate for web.xml setting
+	 
+	<filter>
+	<filter-name>springSecurityFilterChain</filter-name>
+	<filter-class>
+	org.springframework.web.filter.DelegatingFilterProxy
+	</filter-class>
+	</filter>
+*/
+
+}
