b418ae1e74972eb08b24073aa22c43400911f580
Piyush <piyushprasad2007@gmail.com>
2019-03-10 19:20:11 +0530

    Added security for Restful endpoints

--- src/main/java/com/journaldev/spring/config/SecurityConfig.java
+++ src/main/java/com/journaldev/spring/config/SecurityConfig.java
@@ -1,24 +1,68 @@
 package com.journaldev.spring.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+
+import com.journaldev.spring.error.CustomAccessDeniedHandler;
+import com.journaldev.spring.security.MySavedRequestAwareAuthenticationSuccessHandler;
+import com.journaldev.spring.security.RestAuthenticationEntryPoint;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@ComponentScan("com.journaldev.spring")
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().authenticated().and().formLogin().and().httpBasic(); //This will validate each & every request
+	@Autowired
+   private CustomAccessDeniedHandler accessDeniedHandler;
 
+    @Autowired
+    private RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+
+    @Autowired
+    private MySavedRequestAwareAuthenticationSuccessHandler mySuccessHandler;
+
+    private SimpleUrlAuthenticationFailureHandler myFailureHandler = new SimpleUrlAuthenticationFailureHandler();
+
+    public SecurityConfig() {
+        super();
+        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
+    }
+	
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http
+	    .csrf().disable()
+	    .exceptionHandling()
+	    .authenticationEntryPoint(restAuthenticationEntryPoint)
+	    .and()
+	    .authorizeRequests()
+	    .antMatchers("/springData/**").authenticated()
+	    //.antMatchers("/springData/person**").hasRole("ADMIN")
+	    .and()
+	    .formLogin()
+	    .successHandler(mySuccessHandler)
+	    .failureHandler(myFailureHandler)
+	    .and()
+	    .logout();
+		//http.authorizeRequests().anyRequest().authenticated().and().formLogin().and().httpBasic(); //This will validate each & every request -inbuilt login form
 		// http.authorizeRequests().antMatchers("/person/**").authenticated().and().formLogin().permitAll();//this will look for pattern
+		
 	}
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication().withUser("user").password("password").roles("USER")
 		.and().withUser("admin").password("password").roles("USER", "ADMIN");
+		
+		//auth.userDetailsService(null);
 	}
 }
